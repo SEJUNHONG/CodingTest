05d8a3e6ace40b91ff300541c5a7326beaf72af5
SEJUNHONG <57587248+SEJUNHONG@users.noreply.github.com>
2023-07-10 19:56:54 +0900

    Add files via upload

---
+++ Implementation/피로도.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+k = 80
+dungeons = [[80,20],[50,40],[30,10]]
+
+def solution(k, dungeons):
+    answer = 0
+    
+    for p in permutations(dungeons, len(dungeons)):
+        tmp = k
+        cnt = 0
+        
+        for need, spend in p:
+            if tmp >= need:
+                tmp -= spend
+                cnt +=1
+        answer = max(answer, cnt)
+    return answer
+
+print(solution(k, dungeons))
