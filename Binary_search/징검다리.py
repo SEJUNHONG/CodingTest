f7d241637c1836d49cc7924ba604626494544cde
SEJUNHONG <57587248+SEJUNHONG@users.noreply.github.com>
2023-09-05 20:33:34 +0900

    Add files via upload

---
+++ Binary_search/징검다리.py
@@ -0,0 +1,35 @@
+distance = 25
+rocks = [2, 14, 11, 21, 17]
+n = 2
+
+def solution(distance, rocks, n):
+    answer = 0
+    left = 1
+    right = distance
+
+    rocks.sort()
+    rocks.append(distance)
+
+    while left <= right:
+        mid = (left + right) // 2
+        delete = 0
+        prev_rock = 0 
+
+        for rock in rocks:
+            dist = rock - prev_rock
+            if dist < mid:
+                delete += 1
+                if delete > n:
+                    break
+            else:
+                prev_rock = rock
+        
+        if delete > n:
+            right = mid - 1
+        else:
+            answer = mid
+            left = mid + 1
+
+    return answer
+
+print(solution(distance, rocks, n))
